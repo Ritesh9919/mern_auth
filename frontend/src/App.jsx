15f2bac955c611f3c9fc0f187e667c605e049f07
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-08-29 19:44:57 +0530

    add route and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
 
 function App() {
-  return <div className="text-center">App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="text-center text-yellow-300">Home</div>;
+}
+
+export default Home;
