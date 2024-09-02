2bf4c8bfb65c866e9d1426af6085ba90b01699d5
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-02 13:55:44 +0530

    complete the signin page functionality

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Signin() {
-  return <div>Signin</div>;
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await axios.post("/api/auth/signin", formData);
+
+      setLoading(false);
+      if (response.data.error) {
+        setError(true);
+        return;
+      }
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-semibold my-7">Sign In</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-4 max-w-lg mx-auto"
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-300 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="bg-slate-300 p-3 rounded-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading" : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 justify-center mt-7">
+        <p>Dont Have an account?</p>
+        <Link to="/sign-up">
+          <span className="text-blue-500">Sign Up</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5 text-center">
+        {error && "Internal server error"}
+      </p>
+    </div>
+  );
 }
 
 export default Signin;
