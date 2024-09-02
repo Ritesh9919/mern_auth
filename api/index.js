27da56a31314ab9c0b3d8ca9353362d3c13258f7
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-02 11:38:18 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -4,9 +4,13 @@ import express from "express";
 import mongoose from "mongoose";
 import connectDB from "./src/db/index.js";
 
+import userRouter from "./src/routes/user.route.js";
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+
+app.use("/api/users", userRouter);
 
 connectDB()
   .then(() => {
