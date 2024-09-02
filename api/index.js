107851dcd4adef5e9db19e75d7125bc3330c1eff
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-02 11:25:31 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
+import mongoose from "mongoose";
+import connectDB from "./src/db/index.js";
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log(`Server is running on port:${8000}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
