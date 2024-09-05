d58089e6419b11f4123cb590d8d9223785335c37
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-05 14:58:06 +0530

    fixes bugs

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
 const app = express();
-app.use(express.static(path.join(__dirname, "/frontent/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
